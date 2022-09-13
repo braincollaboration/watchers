8fca6f1dd8c6a9368378cf31abac8b318ddfca85
gurprog <ilyagur19999@yandex.ru>
2022-09-13 20:50:14 +0300

    refactoring

--- src/Watchers.WebApi/Bots/BotMessages.cs
+++ src/Watchers.WebApi/Bots/BotMessages.cs
@@ -1,9 +1,9 @@
 ﻿namespace Watchers.WebApi.Bots
 {
-    public class BotMessages
+    public static class BotMessages
     {
-        public static string messageAddedProduct = "Товар занесен в список опрашиваемых";
-        public static string messageNotFound = "Товар занесен в список опрашиваемых";
-        public static string messageWebPageAlreadyExists = "Товар уже занесен в список опрашиваемых";
+        public const string messageAddedProduct = "Товар занесен в список опрашиваемых";
+        public const string messageNotFound = "Товар занесен в список опрашиваемых";
+        public const string messageWebPageAlreadyExists = "Товар уже занесен в список опрашиваемых";
     }
 }
